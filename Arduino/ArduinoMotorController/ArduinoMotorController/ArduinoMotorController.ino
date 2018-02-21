fd29b11f98158bb4ba3f02b9923f72a98b9d8034
Cody Salyer <codysalyer@live.com>
2018-02-21 15:23:13 -0700

    Created both c# application and Arduino application. Code we need to brought into the new motor controller application

---
+++ Arduino/ArduinoMotorController/ArduinoMotorController/ArduinoMotorController.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		ArduinoMotorController.ino
+ Created:	2/21/2018 3:21:51 PM
+ Author:	C Salyer
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() 
+{
+  
+}
